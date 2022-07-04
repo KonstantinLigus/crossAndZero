b69b063125e460ce16e3b92c2f3df4e97c030d76
Konstantin Ligus <kostya1989com@gmail.com>
2022-07-04 17:06:44 +0300

    add new files

---
+++ index.js
@@ -0,0 +1,10 @@
+let counter = 0;
+const refs = {
+  container: document.querySelector(".container"),
+};
+
+refs.container.addEventListener("click", onContainerClick);
+
+function onContainerClick(event) {
+// if(event.target)
+// }
